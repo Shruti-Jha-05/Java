d53a9f912ae145c29c7db457446b0428cf77c377
Shruti Jha <131691811+Shruti-Jha-05@users.noreply.github.com>
2023-11-18 23:57:16 +0530

    Create JavaBasics.java

---
+++ JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.print("Hello World \t");
+        System.out.println("Shruti");
+        System.out.print("hii\n");
+        System.out.print("hello");
+    }
+}
